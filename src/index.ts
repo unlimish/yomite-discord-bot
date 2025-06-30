30dd17335c8efabe0aba7523c272a9660b60b356
unlimish <14168376+unlimish@users.noreply.github.com>
2025-07-01 04:07:13 +0900

    fix: Update bot activity status text to '日記'

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
   console.log(`Ready! Logged in as ${client.user?.tag}`);
-  client.user?.setActivity('土佐日記', { type: ActivityType.Playing });
+  client.user?.setActivity('日記', { type: ActivityType.Playing });
 });
 
 client.on('interactionCreate', async (interaction) => {
