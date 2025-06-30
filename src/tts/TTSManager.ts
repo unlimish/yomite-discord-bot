b73e365d057f56cbdf12f33f34a1d3ccdebe09f4
unlimish <14168376+unlimish@users.noreply.github.com>
2025-06-30 23:58:50 +0900

    fix: Strip unicode emojis to prevent TTS errors

--- src/tts/TTSManager.ts
+++ src/tts/TTSManager.ts
@@ -14,8 +14,6 @@ import { getDictionary } from "../data/dictionary";
 import { getSettings } from "../data/settings";
 import { getUserSettings } from "../data/userSettings";
 
-const emoji = require("node-emoji");
-
 interface TTSQueueItem {
   text: string;
   userId: string;
@@ -122,6 +120,11 @@ export class TTSManager {
     if (serverSettings.readStandardEmojis) {
       text = text.replace(/:([\w_]+):/g, (match, p1) => p1.replace(/_/g, " "));
     }
+
+    // Strip remaining unicode emojis
+    const emojiRegex =
+      /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu;
+    text = text.replace(emojiRegex, "");
 
     const dictionary = getDictionary(this.guild.id);
     for (const word in dictionary) {
